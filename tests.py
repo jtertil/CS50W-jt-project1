9dfc0db5620be112c1f35ecd6bc13b8628d1b6be
jtertil <jtertil@gmail.com>
2019-10-03 20:01:33 +0200

    book details page init;

--- tests.py
+++ tests.py
@@ -13,27 +13,47 @@ class TestViews(unittest.TestCase):
 
     def test_index_get_response(self):
         r = self.app.get('/')
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(200, r.status_code)
 
     def test_index_post_response(self):
         r = self.app.post('/')
-        self.assertEqual(r.status_code, 405)
+        self.assertEqual(405, r.status_code)
 
     def test_login_get_response(self):
         r = self.app.get('/login')
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(200, r.status_code)
 
     def test_login_post_response(self):
         r = self.app.post('/login')
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(200, r.status_code)
 
     def test_register_get_response(self):
         r = self.app.get('/register')
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(200, r.status_code)
 
     def test_register_post_response(self):
         r = self.app.post('/register')
-        self.assertEqual(r.status_code, 200)
+        self.assertEqual(200, r.status_code)
+
+    def test_logout_get_response(self):
+        r = self.app.get('/logout')
+        self.assertEqual(302, r.status_code)
+
+    def test_logout_post_response(self):
+        r = self.app.post('/logout')
+        self.assertEqual(405, r.status_code)
+
+    def test_book_404_response(self):
+        r = self.app.get('/book/fakeisbn')
+        self.assertEqual(404, r.status_code)
+
+    def test_book_get_response(self):
+        r = self.app.get('/book/0380795272')
+        self.assertEqual(200, r.status_code)
+
+    def test_book_post_response(self):
+        r = self.app.post('/book/0380795272')
+        self.assertEqual(405, r.status_code)
 
 
 if __name__ == '__main__':
--- views.py
+++ views.py
@@ -1,4 +1,5 @@
-from flask import request, render_template, redirect, url_for, session, flash
+from flask import request, render_template, redirect, url_for, session,\
+    flash, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from application import app, db
@@ -72,3 +73,35 @@ def logout():
     session['user'] = None
     flash(f"logged as {session['user']}", 'debug')
     return redirect(url_for('index'))
+
+
+@app.route('/book/<string:book_isbn>')
+def book(book_isbn):
+    book = db.execute(
+        'SELECT * FROM public.book '
+        'WHERE isbn = :isbn',
+        {'isbn': book_isbn}
+    ).fetchone()
+
+    if not book:
+        abort(404)
+
+    query_author_ids = db.execute(
+        'SELECT author_id FROM public.book_author '
+        'WHERE book_id = :book_id',
+        {'book_id': book.id}
+    ).fetchall()
+
+    author_ids = [a[0] for a in query_author_ids]
+
+    authors = []
+    for author_id in author_ids:
+        q_a = db.execute(
+            'SELECT name FROM public.author '
+            'WHERE id = :author_id',
+            {'author_id': author_id}
+        ).first()
+        authors.append(q_a[0])
+
+    return "BookID: {}, ISBN: {}, Title: {}, Author(s): {}, Year: {}".format(
+        book.id, book.isbn, book.title, ", ".join(authors), book.year)
